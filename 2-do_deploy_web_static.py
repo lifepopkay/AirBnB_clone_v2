b0f1c1662178ddc0e82f5aab886abb4dfacdc7cd
lifepopkay <popoolafolas@gmail.com>
2024-05-24 04:30:17 -0700

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # This script distributes an archive to my web servers
+
 from fabric.api import env, put, run
 import os
 env.hosts = ['35.153.51.52', '34.229.189.140']
@@ -22,24 +23,24 @@ def do_deploy(archive_path):
         file_name = archive_path.split("/")[-1]
         name = file_name.split(".")[0]
 
-         path = "/data/web_static/releases/"
-         sym_path = "/data/web_static/current"
-         put(archive_path, '/tmp/')
+        path = "/data/web_static/releases/"
+        sym_path = "/data/web_static/current"
+        put(archive_path, '/tmp/')
 
-         # Uncompress the archive to the folder
-         run('mkdir -p {}/{}/'.format(path, name))
-         run('tar -zxf /tmp/{} -C {}/{}'.format(file_name, path, name))
-         
-         # Delete the archive from the web server
-         run('rm /tmp/{}'.format(file_name))
+        # Uncompress the archive to the folder
+        run('mkdir -p {}/{}/'.format(path, name))
+        run('tar -zxf /tmp/{} -C {}/{}'.format(file_name, path, name))
 
-         # Delete the symbolic link /data/web_static/current
-         # from the web server
-         run('rm {}'.format(sym_path))
+        # Delete the archive from the web server
+        run('rm /tmp/{}'.format(file_name))
 
-         # Create a new the symbolic link
-         run('ln -s {}/{}/ {}'.format(path, name, sym_path))
-         
-         return True
+        # Delete the symbolic link /data/web_static/current
+        # from the web server
+        run('rm {}'.format(sym_path))
+
+        # Create a new the symbolic link
+        run('ln -s {}/{}/ {}'.format(path, name, sym_path))
+
+        return True
     except Exception as e:
         return False
