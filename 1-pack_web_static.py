6b061824bb517fb1ec530c56105acf33156d8856
lifepopkay <popoolafolas@gmail.com>
2024-05-24 03:35:20 -0700

    task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-#compress a file before sending
+# Fabfile to generates a .tgz archive from the contents of web_static.
 from fabric.api import run, local
 from datetime import datetime
 
 
 def do_pack():
- """Generates a .tgz archive from the contents of the web_static folder.
+    """Generates a .tgz archive from the contents of the web_static folder.
 
-  Returns:
-      str: The path to the generated archive if successful, None otherwise.
-  """
-  current = datetime.utcnow()
-  file_name = f"web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz"
+       Returns:
+             str: The path to the generated archive if successful,
+             None otherwise.
+    """
+    current = datetime.utcnow()
+    file_name = f"web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz"
 
-  #check if versions exist
-  run("mkdir -p versions", shell=True)
+    # check if versions exist
+    run("mkdir -p versions", shell=True)
 
-  file_tar = local("tar -cvzf versions/{} web_static".format(file_name))
+    file_tar = local("tar -cvzf versions/{} web_static".format(file_name))
 
-  if file_tar is not None:
-      return file_name
-  else:
-      return None
-
+    if file_tar is not None:
+        return file_name
+    else:
+        return None
