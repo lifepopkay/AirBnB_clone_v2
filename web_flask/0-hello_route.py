95d0ce90d5e76d1cd9b42eaa7ae637b8ac6ae191
lifepopkay <popoolafolas@gmail.com>
2024-06-05 22:38:12 +0100

    task

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pythons
+"""
+    a script that starts
+    flask web application
+    Returns:
+            Hello HBNB!
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "<p>Hello HBNB!</p>"
