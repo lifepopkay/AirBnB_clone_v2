4409abd984011450daf41c792df91f527a360411
lifepopkay <popoolafolas@gmail.com>
2024-06-05 22:46:50 +0100

    tasks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,16 @@
     Returns:
             Hello HBNB!
 """
-
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     return "<p>Hello HBNB!</p>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
