1b419c855092d076be9c8ac1ff907af837ce916e
lifepopkay <popoolafolas@gmail.com>
2024-06-05 15:33:14 -0700

    task

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,14 +10,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 @app.route("/hbnb", strict_slashes=False)
 def hello_hbnb():
     return "<p>Hello HBNB!</p>"
 
-@app.route("/c/<text>",strict_slashes=False)
+
+@app.route("/c/<text>", strict_slashes=False)
 def text_to_display(text):
     return f"C {escape(text)}"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
