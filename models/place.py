7d2811e78a1f89fbb0e0eaf3251f6cfef8767612
lifepopkay <popoolafolas@gmail.com>
2024-06-07 13:07:15 +0100

    drop

--- models/place.py
+++ models/place.py
@@ -7,17 +7,15 @@ import sqlalchemy
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
-if models.storage_t == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id', onupdate='CASCADE',
-                                            ondelete='CASCADE'),
-                                 primary_key=True),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id', onupdate='CASCADE',
-                                            ondelete='CASCADE'),
-                                 primary_key=True))
-
+if getenv("HBNB_TYPE_STORAGE") != "db":
+    association_table = Table('place_amenity', Base.metadata,
+                              Column('place_id', String(60), ForeignKey(
+                                  'places.id'), nullable=False,
+                                  primary_key=True),
+                              Column('amenity_id', String(60), ForeignKey(
+                                  'amenities.id'), nullable=False,
+                                  primary_key=True)
+                              )
 
 class Place(BaseModel, Base):
     """Representation of Place """
@@ -34,7 +32,7 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place")
-    amenities = relationship("Amenity", secondary="place_amenity",
+    amenities = relationship("Amenity", secondary=association_table,
                                  backref="place_amenities",
                                  viewonly=False)
 
