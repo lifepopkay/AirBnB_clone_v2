d1b951fd357c1522bba82c8f5c0d15cad6129a30
lifepopkay <popoolafolas@gmail.com>
2024-06-07 04:29:44 -0700

    task

--- models/place.py
+++ models/place.py
@@ -7,7 +7,7 @@ import sqlalchemy
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
-if models.storage_t == 'db':
+if getenv("HBNB_TYPE_STORAGE") != "db":
     place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60),
                                  ForeignKey('places.id', onupdate='CASCADE',
@@ -22,8 +22,7 @@ if models.storage_t == 'db':
 class Place(BaseModel, Base):
     """Representation of Place """
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
-        __tablename__ = 'places'
+    if getenv("H        __tablename__ = 'places'
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
