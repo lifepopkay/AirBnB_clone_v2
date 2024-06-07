0e3b9edbe5a6c283512660dbb95bc744af7ddd96
lifepopkay <popoolafolas@gmail.com>
2024-06-07 12:26:55 +0100

    merge

--- models/place.py
+++ models/place.py
@@ -22,9 +22,10 @@ if models.storage_t == 'db':
 class Place(BaseModel, Base):
     """Representation of Place """
  
-__tablename__ = 'places'
+    __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)        name = Column(String(128), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
