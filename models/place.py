fd5cf1b00a156821305d49e80e30451b22e596f3
lifepopkay <popoolafolas@gmail.com>
2024-06-07 13:30:53 +0100

    Changes

--- models/place.py
+++ models/place.py
@@ -7,15 +7,17 @@ import sqlalchemy
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
-if getenv("HBNB_TYPE_STORAGE") != "db":
-    association_table = Table('place_amenity', Base.metadata,
-                              Column('place_id', String(60), ForeignKey(
-                                  'places.id'), nullable=False,
-                                  primary_key=True),
-                              Column('amenity_id', String(60), ForeignKey(
-                                  'amenities.id'), nullable=False,
-                                  primary_key=True)
-                              )
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True,
+                             nullable=False),
+                      Column('amenity_id', String(60), 
+                             ForeignKey('amenities.id'), 
+                             primary_key=True,
+                             nullable=False)
+                    )
 
 class Place(BaseModel, Base):
     """Representation of Place """
@@ -31,10 +33,13 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    reviews = relationship("Review", backref="place")
-    amenities = relationship("Amenity", secondary="association_table",
-                                 back_populates="place_amenities",
-                                 viewonly=False)
+    
+    if getenv("HBNB_TYPE_STORAGE") == "db":  
+        reviews = relationship("Review", backref="place")
+        
+        amenities = relationship("Amenity", secondary="association_table",
+                                    back_populates="place_amenities",
+                                    viewonly=False)
 
     
     def __init__(self, *args, **kwargs):
